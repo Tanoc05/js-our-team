c8486eb370ffe2f8cc3d7908b4a2aac0ad76ccb8
Gaetano Calabrò <calabrogaetano5@gmail.com>
2026-02-22 18:08:42 +0100

    sec.commit

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,94 @@
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "assets/img/female3.png"
   }
 ];
 
-teamMembers.forEach(element => {
-  const el = document.querySelector(".blnks");
-  el.append(element.email);
-});
+const container = document.querySelector(".card-container");
+
+for (let index = 0; index < teamMembers.length; index++) {
+  
+  const {name,role,email,img} = teamMembers[index];
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+
+  const cardImage = document.createElement("div");
+  cardImage.classList.add("card-image");
+  const cardImageSrc = document.createElement("img");
+  cardImageSrc.src = img;
+
+  cardImage.appendChild(cardImageSrc);
+
+  const cardName = document.createElement("div");
+  cardName.classList.add("card-name");
+  const cardNameP = document.createElement("p");
+  cardNameP.innerHTML = name;
+
+  cardName.appendChild(cardNameP);
+
+  
+  const cardRole = document.createElement("div");
+  cardRole.classList.add("card-role");
+  const cardRolep = document.createElement("p");
+  cardRolep.innerHTML = role;
+
+  cardRole.appendChild(cardRolep);
+
+  const cardEmail = document.createElement("div");
+  cardEmail.classList.add("card-email");
+  const cardEmailP = document.createElement("p");
+  cardEmailP.innerHTML = email;
+
+  cardEmail.appendChild(cardEmailP);
+
+  cardInfo.appendChild(cardName)
+  cardInfo.appendChild(cardRole)
+  cardInfo.appendChild(cardEmail)
+  
+  card.appendChild(cardImage);
+  card.appendChild(cardInfo);
+
+  container.appendChild(card);
+
+}
+
+
+
